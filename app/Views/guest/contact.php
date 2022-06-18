6817248d18d6facae5a1fe39d3325ca2ed066d3f
Ditio <firdausditio@student.uns.ac.id>
2022-06-18 14:19:34 +0700

    nambahin guest

---
+++ app/Views/guest/contact.php
@@ -0,0 +1,81 @@
+<?= $this->extend('guest/templates/main'); ?>
+<?= $this->section('content'); ?>
+<section class="pt-7" id="about">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-8 text-md-start text-center py-6">
+                <h1 class="mb-4 fs-9 fw-bold">S Labs</h1>
+                <p class="mb-6 lead text-secondary">Laboratories used for scientific research vary in form due to the different requirements for specialists in different fields of science and engineering. An animation lab may contain computers and advanced animation tools, while a chemistry lab may have full chemistry equipment. and computer laboratories can have above-average computers to support learning. S labs use laboratories to make it easier for the community and help add new knowledge without having to spend a lot of money.</p>
+                <div class="text-center text-md-start"><a class="btn btn-warning me-3 btn-lg" href="index.html" role="button">Get started</a></div>
+            </div>
+            <div class="col-md-4 text-end">
+                <center><img class="pt-7 pt-md-0 img-fluid" src="https://i0.wp.com/uns.ac.id/id/wp-content/uploads/logo-uns-biru.png?fit=528%2C526&ssl=1" width="75%" alt="" /></center>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="pt-5" id="contact">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2">,<center><img class="img-fluid" src="member-assets/img/icons/call.png" width="38%" alt="" /></center>
+            </div>
+            <div class="col-lg-4">
+                <br>
+                <h4>Call and Whatsapp</h4><br>
+                <div class="d-flex align-items-center mb-3"> <img class="me-sm-4 me-2" src="member-assets/img/manager/tick.png" width="25" alt="tick" />
+                    <a class="link-900 text-secondary text-decoration-none" href="https://wa.me/08123655411">08123655411 (Major)</a>
+                </div>
+                <div class="d-flex align-items-center mb-3"> <img class="me-sm-4 me-2" src="member-assets/img/manager/tick.png" width="25" alt="tick" />
+                    <a class="link-900 text-secondary text-decoration-none" href="https://wa.me/08123655422">08123655422 (Sintara)</a>
+                </div>
+                <div class="d-flex align-items-center mb-3"> <img class="me-sm-4 me-2" src="member-assets/img/manager/tick.png" width="25" alt="tick" />
+                    <a class="link-900 text-secondary text-decoration-none" href="https://wa.me/08123655433">08123655433 (Sludge)</a>
+                </div>
+                <div class="d-flex align-items-center mb-3"> <img class="me-sm-4 me-2" src="member-assets/img/manager/tick.png" width="25" alt="tick" />
+                    <a class="link-900 text-secondary text-decoration-none" href="https://wa.me/08123655444">08123655444 (Astra)</a>
+                </div>
+            </div>
+            <div class="col-lg-2">,<center><img class="img-fluid" src="member-assets/img/icons/mail-outline.png" width="38%" alt="" /></center>
+            </div>
+            <div class="col-lg-4">
+                <br>
+                <h4>Email</h4><br>
+                <div class="d-flex align-items-center mb-3"> <img class="me-sm-4 me-2" src="member-assets/img/manager/tick.png" width="25" alt="tick" />
+                    <a class="link-900 text-secondary text-decoration-none" href="mailto: slabsadmin@uns.ac.id">slabsadmin@uns.ac.id (Major)</a>
+                </div>
+                <div class="d-flex align-items-center mb-3"> <img class="me-sm-4 me-2" src="member-assets/img/manager/tick.png" width="25" alt="tick" />
+                    <a class="link-900 text-secondary text-decoration-none" href="mailto: slabssintara@uns.ac.id">slabssintara@uns.ac.id (Sintara)</a>
+                </div>
+                <div class="d-flex align-items-center mb-3"> <img class="me-sm-4 me-2" src="member-assets/img/manager/tick.png" width="25" alt="tick" />
+                    <a class="link-900 text-secondary text-decoration-none" href="mailto: slabssludge@uns.ac.id">slabssludge@uns.ac.id (Sludge)</a>
+                </div>
+                <div class="d-flex align-items-center mb-3"> <img class="me-sm-4 me-2" src="member-assets/img/manager/tick.png" width="25" alt="tick" />
+                    <a class="link-900 text-secondary text-decoration-none" href="mailto: slabsastra@uns.ac.id">slabsastra@uns.ac.id (Astra)</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="pt-5" id="validation">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2">,<center><img class="img-fluid" src="member-assets/img/icons/location-on.png" width="38%" alt="" /></center>
+            </div>
+            <div class="col-lg-4">
+                <br>
+                <h4>Location</h4><br>
+                <div class="d-flex align-items-center mb-3"> <img class="me-sm-4 me-2" src="member-assets/img/manager/tick.png" width="25" alt="tick" />
+                    <h5 class="fw-medium mb-0 text-secondary">Jl. Ir. Sutami No.36, Kentingan, <br />Kec. Jebres, Kota Surakarta, <br />Jawa Tengah 57126</h5>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <center><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15820.564931360708!2d110.8563871!3d-7.5595759!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xbda63b32997616ad!2sUniversitas%20Sebelas%20Maret!5e0!3m2!1sid!2sid!4v1655131942208!5m2!1sid!2sid" width="80%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></center>
+            </div>
+        </div>
+    </div>
+</section>
+<?= $this->endSection('content'); ?>
---
+++ app/Views/guest/labroom.php
@@ -0,0 +1,43 @@
+<?= $this->extend('guest/templates/main'); ?>
+<?= $this->section('content'); ?>
+
+<section class="pt-5" id="marketing">
+
+    <div class="container">
+        <center>
+            <h1 class="fw-bold fs-6 mb-3">Kategori Laboratorium</h1>
+        </center>
+        <p class="mb-6 text-secondary">Join this laboratory</p>
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <div class="card"><a class="navbar-brand" href="#"><img class="card-img-top" src="member-assets/img/marketing/marketing04.jpg" alt="" />
+                        <div class="card-body ps-0">
+                            <p class="text-secondary">By <a class="fw-bold text-decoration-none me-1" href="home">Hasan</a>|<span class="ms-1">19 May 2022</span></p>
+                            <h3 class="fw-bold">Sintara</h3>
+                        </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card"><a class="navbar-brand" href="#"><img class="card-img-top" src="member-assets/img/marketing/marketing05.jpg" alt="" />
+                        <div class="card-body ps-0">
+                            <p class="text-secondary">By <a class="fw-bold text-decoration-none me-1" href="#">Abdullah</a>|<span class="ms-1">11 May 2022</span></p>
+                            <h3 class="fw-bold">Sludge</h3>
+                        </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card"><a class="navbar-brand" href="#"><img class="card-img-top" src="member-assets/img/marketing/marketing06.jpg" alt="" />
+                        <div class="card-body ps-0">
+                            <p class="text-secondary">By <a class="fw-bold text-decoration-none me-1" href="#">Ibnu</a>|<span class="ms-1">03 May 2022</span></p>
+                            <h3 class="fw-bold">Astra</h3>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end of .container-->
+
+</section>
+<!-- <section> close ============================-->
+<!-- ============================================-->
+<?= $this->endSection('content'); ?>
